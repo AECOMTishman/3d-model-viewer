6ec935830a766815be0af699a15dc86822a88d0e
Joseph McMahon <jmcmahon443@gmail.com>
2015-04-23 15:50:46 -0400

    sad

--- js/main2.js
+++ js/main2.js
@@ -1,109 +1,137 @@
-var container;
-var camera, scene, renderer;
-var mouseX = 0, mouseY = 0;
 
-var WIDTH = window.innerWidth;
-var HEIGHT = window.innerWidth;
- 
-var windowHalfX = WIDTH / 2;
-var windowHalfY = HEIGHT / 2;
- 
-init();
-animate();
- 
-/*** Initialize ***/
-function init() {
-  // This <div> will host the canvas for our scene.
-  container = document.getElementById( '3d' );
-  document.body.appendChild( container );
- 
-  // You can adjust the cameras distance and set the FOV to something
-  // different than 45°. The last two values set the clippling plane.
-  camera = new THREE.PerspectiveCamera( 45, WIDTH / HEIGHT, 1, 2000 );
-  camera.position.z = 100;
- 
-  // These variables set the camera behaviour and sensitivity.
-  controls = new THREE.TrackballControls( camera );
-  controls.rotateSpeed = 5.0;
-  controls.zoomSpeed = 5;
-  controls.panSpeed = 2;
-  controls.noZoom = false;
-  controls.noPan = false;
-  controls.staticMoving = true;
-  controls.dynamicDampingFactor = 0.3;
- 
-  // This is the scene we will add all objects to.
-  scene = new THREE.Scene();
- 
-  // You can set the color of the ambient light to any value.
-  // I have chose a completely white light because I want to paint
-  // all the shading into my texture. You propably want something darker.
-  var ambient = new THREE.AmbientLight( 0xffffff );
-  scene.add( ambient );
- 
-  // Uncomment these lines to create a simple directional light.
-  // var directionalLight = new THREE.DirectionalLight( 0xffeedd );
-  // directionalLight.position.set( 0, 0, 1 ).normalize();
-  // scene.add( directionalLight );
- 
-  /*** Texture Loading ***/
-  var manager = new THREE.LoadingManager();
-  manager.onProgress = function ( item, loaded, total ) {
-    console.log( item, loaded, total );
-  };
-  var texture = new THREE.Texture();
-  var loader = new THREE.ImageLoader( manager );
- 
-  // You can set the texture properties in this function. 
-  // The string has to be the path to your texture file.
-  loader.load( 'medieval-house-2/DoorType1_1.jpg', function ( image ) {
-    texture.image = image;
-    texture.needsUpdate = true;
-    // I wanted a nearest neighbour filtering for my low-poly character,
-    // so that every pixel is crips and sharp. You can delete this lines
-    // if have a larger texture and want a smooth linear filter.
-    texture.magFilter = THREE.NearestFilter;
-    texture.minFilter = THREE.NearestMipMapLinearFilter;
-  } );
- 
-  /*** OBJ Loading ***/
-  var loader = new THREE.OBJLoader( manager );
- 
-  // As soon as the OBJ has been loaded this function looks for a mesh
-  // inside the data and applies the texture to it.
-  loader.load( 'medieval-house-2/medieval-house-2.obj', function ( event ) {
-    var object = event;
-    object.traverse( function ( child ) {
-      if ( child instanceof THREE.Mesh ) {
-        child.material.map = texture;
+      var container;
+
+      var camera, scene, renderer;
+
+      var mouseX = 0, mouseY = 0;
+
+      var windowHalfX = window.innerWidth / 2;
+      var windowHalfY = window.innerHeight / 2;
+
+
+      init();
+      animate();
+
+
+      function init() {
+
+        container = document.getElementById( '3d' );
+        document.body.appendChild( container );
+
+        camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+        camera.position.z = 100;
+
+        // scene
+
+        scene = new THREE.Scene();
+
+        var ambient = new THREE.AmbientLight( 0x101030 );
+        scene.add( ambient );
+
+        var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+        directionalLight.position.set( 0, 0, 1 );
+        scene.add( directionalLight );
+
+        // texture
+
+        var manager = new THREE.LoadingManager();
+        manager.onProgress = function ( item, loaded, total ) {
+
+          console.log( item, loaded, total );
+
+        };
+
+        var texture = new THREE.Texture();
+
+        var onProgress = function ( xhr ) {
+          if ( xhr.lengthComputable ) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log( Math.round(percentComplete, 2) + '% downloaded' );
+          }
+        };
+
+        var onError = function ( xhr ) {
+        };
+
+
+        var loader = new THREE.ImageLoader( manager );
+        loader.load( 'textures/UV_Grid_Sm.jpg', function ( image ) {
+
+          texture.image = image;
+          texture.needsUpdate = true;
+
+        } );
+
+        // model
+
+        var loader = new THREE.OBJLoader( manager );
+        loader.load( 'obj/male02/male02.obj', function ( object ) {
+
+          object.traverse( function ( child ) {
+
+            if ( child instanceof THREE.Mesh ) {
+
+              child.material.map = texture;
+
+            }
+
+          } );
+
+          object.position.y = - 80;
+          scene.add( object );
+
+        }, onProgress, onError );
+
+        //
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        container.appendChild( renderer.domElement );
+
+        document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+        //
+
+        window.addEventListener( 'resize', onWindowResize, false );
+
       }
-    } );
- 
-    // My initial model was too small, so I scaled it upwards.
-    object.scale = new THREE.Vector3( 25, 25, 25 );
- 
-    // You can change the position of the object, so that it is not
-    // centered in the view and leaves some space for overlay text.
-    object.position.y -= 2.5;
-    scene.add( object );
-  });
- 
-  // We set the renderer to the size of the window and
-  // append a canvas to our HTML page.
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize( WIDTH, HEIGHT );
-  container.appendChild( renderer.domElement );
-}
- 
-/*** The Loop ***/
-function animate() {
-  // This function calls itself on every frame. You can for example change
-  // the objects rotation on every call to create a turntable animation.
-  requestAnimationFrame( animate );
- 
-  // On every frame we need to calculate the new camera position
-  // and have it look exactly at the center of our scene.
-  controls.update();
-  camera.lookAt(scene.position);
-  renderer.render(scene, camera);
-}
+
+      function onWindowResize() {
+
+        windowHalfX = window.innerWidth / 2;
+        windowHalfY = window.innerHeight / 2;
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+      }
+
+      function onDocumentMouseMove( event ) {
+
+        mouseX = ( event.clientX - windowHalfX ) / 2;
+        mouseY = ( event.clientY - windowHalfY ) / 2;
+
+      }
+
+      //
+
+      function animate() {
+
+        requestAnimationFrame( animate );
+        render();
+
+      }
+
+      function render() {
+
+        camera.position.x += ( mouseX - camera.position.x ) * .05;
+        camera.position.y += ( - mouseY - camera.position.y ) * .05;
+
+        camera.lookAt( scene.position );
+
+        renderer.render( scene, camera );
+
+      }
