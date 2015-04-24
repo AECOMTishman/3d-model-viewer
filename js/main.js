860607984be8a5f370af87a452a1c7a54b277add
Tishman Construction <joseph.mcmahon@aecom.com>
2015-04-24 14:54:09 -0400

    one more zoom attempt

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ function render() {
 
   camera.position.x = ( mouseX / WIDTH ) * 1000 - 500;
   camera.position.y = 2000 - ( ( mouseY / HEIGHT ) * 1000 - 500 );
-  camera.position.z = 2000 + ( ( mouseX / WIDTH ) * 1000 - 500 );
+  camera.position.z = 2000 + Math.abs( ( mouseX / WIDTH ) * 1000 - 500 );
 
   camera.lookAt( scene.position );
 
