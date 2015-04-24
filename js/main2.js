9db73e72bd396222b0a6f6994b92f1aa6dbbda2f
Tishman Construction <joseph.mcmahon@aecom.com>
2015-04-24 17:18:12 -0400

    test

--- js/main2.js
+++ js/main2.js
@@ -1,50 +1,101 @@
-var viewport, scene, camera, renderer, loader, controls;
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
-var WIDTH = 498, /*window.innerWidth*/
-    HEIGHT = 498; /*window.innerHeight*/
+var container, stats;
 
-var VIEW_ANGLE = 45,
-    ASPECT = WIDTH / HEIGHT,
-    NEAR = 1,
-    FAR = 10000;
+var camera, controls, scene, renderer;
 
-viewport = document.getElementById( '3d' );
-
-scene = new THREE.Scene();
-
-camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-camera.position.y = 1000;
-scene.add(camera);
-
-renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.shadowMapEnabled = true;
-renderer.shadowMapSoft = true;
-renderer.shadowMapType = THREE.PCFShadowMap;
-renderer.shadowMapAutoUpdate = true;
-
-controls = new THREE.OrbitControls(camera);
-
-renderer.setSize(WIDTH, HEIGHT);
-
-loader = new THREE.JSONLoader();
-
-loader.load('sample.js', function (geometry, materials) {
-  var mesh, material;
-
-  material = new THREE.MeshFaceMaterial(materials);
-  mesh = new THREE.Mesh(geometry, material);
-
-  mesh.scale.set(1, 1, 1);
-  mesh.receiveShadow = true;
-  mesh.castShadow = true;
-
-  scene.add(mesh);
-});
-
-animate();
+init();
+render();
 
 function animate() {
-  renderer.render(scene, camera);
-  controls.update();
+
   requestAnimationFrame(animate);
+  controls.update();
+
+}
+
+function init() {
+
+  camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+  camera.position.z = 500;
+
+  controls = new THREE.OrbitControls( camera );
+  controls.damping = 0.2;
+  controls.addEventListener( 'change', render );
+
+  scene = new THREE.Scene();
+  scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+
+  // world
+
+  var geometry = new THREE.CylinderGeometry( 0, 10, 30, 4, 1 );
+  var material =  new THREE.MeshLambertMaterial( { color:0xffffff, shading: THREE.FlatShading } );
+
+  for ( var i = 0; i < 500; i ++ ) {
+
+    var mesh = new THREE.Mesh( geometry, material );
+    mesh.position.x = ( Math.random() - 0.5 ) * 1000;
+    mesh.position.y = ( Math.random() - 0.5 ) * 1000;
+    mesh.position.z = ( Math.random() - 0.5 ) * 1000;
+    mesh.updateMatrix();
+    mesh.matrixAutoUpdate = false;
+    scene.add( mesh );
+
+  }
+
+
+  // lights
+
+  light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( 1, 1, 1 );
+  scene.add( light );
+
+  light = new THREE.DirectionalLight( 0x002288 );
+  light.position.set( -1, -1, -1 );
+  scene.add( light );
+
+  light = new THREE.AmbientLight( 0x222222 );
+  scene.add( light );
+
+
+  // renderer
+
+  renderer = new THREE.WebGLRenderer( { antialias: false } );
+  renderer.setClearColor( scene.fog.color );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  container = document.getElementById( '3d' );
+  container.appendChild( renderer.domElement );
+
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.top = '0px';
+  stats.domElement.style.zIndex = 100;
+  container.appendChild( stats.domElement );
+
+  //
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+  animate();
+
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  render();
+
+}
+
+function render() {
+
+  renderer.render( scene, camera );
+  stats.update();
+
 }
