1a2ca3e291f3ee523bc04806a84aca768c1d96ce
Tishman Construction <joseph.mcmahon@aecom.com>
2015-04-27 12:07:25 -0400

    asd

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ loader = new THREE.JSONLoader();
 var mesh;
 loader.load('sample.js', function (geometry, materials) {  
   mesh = new THREE.Mesh(
-    geometry, THREE.MeshDepthMaterial
+    geometry, new THREE.MeshDepthMaterial()
   );
 
   mesh.rotation.x = -Math.PI / 2;
