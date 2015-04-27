3743617f33d0cbf52e8608a9a32620f4a102994c
Tishman Construction <joseph.mcmahon@aecom.com>
2015-04-27 12:50:10 -0400

    shiny

--- js/main.js
+++ js/main.js
@@ -33,7 +33,7 @@ scene.add(camera);
 loader = new THREE.JSONLoader();
 var mesh;
 loader.load('sample.js', function (geometry, materials) {  
-  material1 = new THREE.MeshDepthMaterial();
+  material1 = new new THREE.MeshPhongMaterial( { color: 0xdddddd, specular: 0x009900, shininess: 30, shading: THREE.FlatShading } );
   material2 = materials;
   console.log(material2);
 
