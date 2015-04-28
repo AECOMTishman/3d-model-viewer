139420da2282c7ea2e0562130d60f6ea83e2bc5c
Tishman Construction <joseph.mcmahon@aecom.com>
2015-04-28 16:06:02 -0400

    all done for the day

--- js/main2.js
+++ js/main2.js
@@ -31,15 +31,6 @@ camera.lookAt(new THREE.Vector3(0, 500, 0));
 
 scene.add(camera);
 
-function animate() {
-	requestAnimationFrame(animate);
-	controls.update();
-}
-
-controls = new THREE.OrbitControls( camera );
-controls.damping = 0.2;
-controls.addEventListener( 'change', render );
-
 hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.6 );
 hemiLight.color.setHSL( 0.6, 1, 0.6 );
 hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
