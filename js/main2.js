d55f92489f3c8068dd8d90545485ab1a15a42397
Tishman Construction <joseph.mcmahon@aecom.com>
2015-04-30 16:36:59 -0400

    fix light buttons

--- js/main2.js
+++ js/main2.js
@@ -113,8 +113,8 @@ $( 'button#light1a' ).addClass('active')
 
 $( 'button#light1a' ).click( function() {
 	$( 'button#light1a' ).addClass('active')
-	$( 'button#light2b' ).removeClass('active')
-	$( 'button#light3c' ).removeClass('active')
+	$( 'button#light1b' ).removeClass('active')
+	$( 'button#light1c' ).removeClass('active')
 	light1.position.set(0, 500, 0);
 	light1.color.setHSL( 0.6, 1, 0.6 );
 	light1.groundColor.setHSL( 0.095, 1, 0.75 );
@@ -122,8 +122,8 @@ $( 'button#light1a' ).click( function() {
 
 $( 'button#light1b' ).click( function() {
 	$( 'button#light1a' ).removeClass('active')
-	$( 'button#light2b' ).addClass('active')
-	$( 'button#light3c' ).removeClass('active')
+	$( 'button#light1b' ).addClass('active')
+	$( 'button#light1c' ).removeClass('active')
 	light1.position.set(0, 1000, 0);
 	light1.color.setHSL( 0.8, 1, 0.8 );
 	light1.groundColor.setHSL( 0.095, 1, 0.75 );
@@ -131,8 +131,8 @@ $( 'button#light1b' ).click( function() {
 
 $( 'button#light1c' ).click( function() {
 	$( 'button#light1a' ).removeClass('active')
-	$( 'button#light2b' ).removeClass('active')
-	$( 'button#light3c' ).addClass('active')
+	$( 'button#light1b' ).removeClass('active')
+	$( 'button#light1c' ).addClass('active')
 	light1.position.set(0, 2000, 0);
 	light1.color.setHSL( 1, 1, 1 );
 	light1.groundColor.setHSL( 0.095, 1, 0.75 );
