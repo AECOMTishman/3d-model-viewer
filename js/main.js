f92f19eef5385c62f2e2ab9c25a3fdd307233907
Tishman Construction <joseph.mcmahon@aecom.com>
2015-05-05 13:44:51 -0400

    orbit limits

--- js/main.js
+++ js/main.js
@@ -34,9 +34,9 @@ function init() {
 
 	controls = new THREE.OrbitControls( camera );
 	controls.addEventListener( 'change', render );
-	this.minDistance = 0;
-	this.maxDistance = 8000;
-	control.maxPolarAngle = Math.PI/2;
+	controls.minDistance = 0;
+	controls.maxDistance = 8000;
+	controls.maxPolarAngle = Math.PI/2;
 
 	camera.position.set(0, 2000, 2000);
 	camera.lookAt(new THREE.Vector3(0, 1000, 0));
