d36754c82e04b245e5802ad090f16e37a8534fd4
Joseph McMahon <jmcmahon443@gmail.com>
2015-05-06 18:34:29 -0400

    fix shadows

--- js/main.js
+++ js/main.js
@@ -185,9 +185,9 @@ $( 'a#view1' ).click( function() {
 });
 
 $( 'a#view2' ).click( function() {
-	camera.position.set( 400, 500, 400 );
-	camera.lookAt(new THREE.Vector3( 0, 500, 0 ));
-	controls.target = new THREE.Vector3( 0, 500, 0 );
+	camera.position.set( 500, 800, 500 );
+	camera.lookAt(new THREE.Vector3( 500, 800, 0 ));
+	controls.target = new THREE.Vector3( 500, 800, 0 );
 	render();
 });
 
