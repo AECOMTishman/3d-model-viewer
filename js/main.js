07713d6f4df3174c0c8f12a8109c26a894510aa2
Tishman Construction <joseph.mcmahon@aecom.com>
2015-05-06 11:30:10 -0400

    fix

--- js/main.js
+++ js/main.js
@@ -61,14 +61,6 @@ function init() {
 	spotLight.shadowCameraVisible	= true;
 	scene.add( spotLight );	
 
-	onRenderFcts.push(function(){
-		var angle	= Date.now()/1000 * Math.PI;
-// angle	= Math.PI*2
-		spotLight.position.x	= Math.cos(angle*-0.1)*20;
-		spotLight.position.y	= 10 + Math.sin(angle*0.5)*6;
-		spotLight.position.z	= Math.sin(angle*-0.1)*20;		
-	})
-
 	group = new THREE.Object3D();
 
 	loader = new THREE.JSONLoader();
