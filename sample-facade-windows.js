e29174f7abb50ea608e548c8a216d4ec291f1e1b
Tishman Construction <joseph.mcmahon@aecom.com>
2015-05-08 10:42:53 -0400

    sample model windows

--- sample-facade-windows.js
+++ sample-facade-windows.js
@@ -20,8 +20,9 @@
 "colorDiffuse"  : [0.9255, 1.0000, 1.0000],
 "colorAmbient"  : [0.9255, 1.0000, 1.0000],
 "colorSpecular"  : [0.9000, 0.9000, 0.9000],
+"traparent"  : true,
 "transparency"  : 0.25,
-"specularCoef"  : 30.0,
+"specularCoef"  : 300.0,
 "vertexColors" : false
 }
 
